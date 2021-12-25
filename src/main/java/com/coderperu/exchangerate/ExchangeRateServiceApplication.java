d14fed7c3e14f0fc8254c6ae2d28550fd5df6c5d
Miguel Mamani <miguel.coder.pe@gmail.com>
2021-12-24 22:21:26 -0500

    Scaffolding of spring-boot project

---
+++ src/main/java/com/coderperu/exchangerate/ExchangeRateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coderperu.exchangerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateServiceApplication.class, args);
+	}
+
+}
